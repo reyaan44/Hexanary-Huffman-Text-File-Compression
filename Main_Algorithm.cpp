823966d253c6ba86cf9b0bc2658c9ac920a09b30
Reyaan Jagnani <reyaanjagnani01@gmail.com>
2022-12-13 17:38:42 +0530

    Variables updated and comments added

--- Main_Algorithm.cpp
+++ Main_Algorithm.cpp
@@ -1,9 +1,13 @@
 // Code by Reyaan Jagnani (reyaan44)
+
 #include<bits/stdc++.h>
+
 #define endl "\n"
 #define quick ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define timetaken cout<<fixed<<setprecision(10); cout << "TOTAL TIME TAKEN : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+
 using namespace std;
+
 #include "Tree_Type.cpp"
 #include "Class_Node.cpp"
 #include "Input_Frequency.cpp"
@@ -11,6 +15,7 @@ using namespace std;
 #include "Construct_Codes.cpp"
 #include "Compressed_File.cpp"
 #include "Decompressed_File.cpp"
+
 int main()
 {
     quick;
@@ -20,6 +25,7 @@ int main()
     freopen("error.txt", "w", stderr);
 #endif
 
+    // Data Variables
     unordered_map<char,int> frequency;
     priority_queue<pair<int,node*>, vector<pair<int,node*>>, greater<pair<int,node*>>> min_heap;
     string input_data;
@@ -30,14 +36,17 @@ int main()
     int input_data_size;
     int compressed_data_size;
     double percentage_compression;
+    node* root;
 
+    // Storing Frequencies with characters
     input_data = readAndPrint("input_data.txt", frequency);
+
+    // Preparing Min_Heap Based on Frequency
     for(auto itr: frequency)
     {
         node* temp = new node(itr.first, itr.second);
         min_heap.push({itr.second,temp});
     }
-    node* root;
     while(min_heap.size()>0)
     {
         if(min_heap.size()==1)
@@ -46,28 +55,31 @@ int main()
             min_heap.pop();
             break;
         }
-        vector<node*> vect;
-        int total = 0;
-        while(min_heap.size()>0 && vect.size()<tree)
+        vector<node*> node_pack;
+        int total_frequecies = 0;
+        while(min_heap.size()>0 && node_pack.size()<tree)
         {
             node* n1 = min_heap.top().second;
             int freq1 = min_heap.top().first;
-            total+=freq1;
-            vect.push_back(n1);
+            total_frequecies+=freq1;
+            node_pack.push_back(n1);
             min_heap.pop();
         }
-        node* temp = new node(total, vect);
-        min_heap.push({total,temp});
+        node* temp = new node(total_frequecies, node_pack);
+        min_heap.push({total_frequecies,temp});
     }
+
+    // Forming code_to_string_mapping and string_to_code_mapping
     dfs(root, "", code_to_string_mapping, string_to_code_mapping);
-    
     for(auto itr: code_to_string_mapping)
         cout<<itr.first<<" "<<itr.second<<endl;
     cout<<endl;
 
-    input_data_size = input_data.size()*8;
+    // 1 character takes 8 bits
+    input_data_size = input_data.size()*8;  
     cout<<"INITIAL SIZE = "<<input_data_size<<" BITS"<<endl<<endl;
 
+    // 1 bit size for each binary bit (0 and 1)
     compressed_data = encrypt(input_data, code_to_string_mapping);
     compressed_data_size = compressed_data.size();
     
